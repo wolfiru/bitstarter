53f3a306bbccabc7c43e3642cda5e9dd8743db2e
Wolfgang Ruthner <wolfgang@ruthner.at>
2013-07-04 16:48:38 +0000

    web.js 3

--- web.js
+++ web.js
@@ -1,9 +1,10 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  fs.readFile('index.html',function(err,data) {
+  fs.readFile('./index.html',function read(err,data) {
      if (err) throw err;
      response.send(data);
   });
