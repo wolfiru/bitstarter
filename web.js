03201f2970c89d36ead2ee2ce208b3b4b9243af2
Wolfgang Ruthner <wolfgang@ruthner.at>
2013-07-04 16:35:41 +0000

    Mod web.js

--- web.js
+++ web.js
@@ -3,7 +3,11 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  fs.readFile('index.html',function(err,data) {
+     if (err) throw err;
+  }
+  var index_content = data.toString();
+  response.send(index_content);
 });
 
 var port = process.env.PORT || 5000;
